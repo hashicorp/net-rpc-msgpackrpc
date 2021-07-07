38fce476b915c7fdf130f10a44b52e30be0cdc37
Daniel Nephin <dnephin@gmail.com>
2021-07-07 15:08:10 -0400

    Update internal imports to net/rpc

--- msgpackrpc.go
+++ msgpackrpc.go
@@ -6,7 +6,7 @@ package msgpackrpc
 import (
 	"io"
 	"net"
-	"net/rpc"
+	"github.com/hashicorp/go-msgpack/net/rpc"
 )
 
 // Dial connects to a MessagePack-RPC server at the specified network address.
